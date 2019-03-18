51f2ddf9c3c2a14e850acf930cce703ceb1b8e79
tanglei <tanglei@intest.cn>
2019-03-18 15:57:03 +0800

    redis

--- count.py
+++ count.py
@@ -53,3 +53,11 @@ print("查询时间范围：%s - %s" % (a, b))
 print("报警条数：%s" % alert_count(redisid, clock(a), clock(b)))
 print("电话报警条数：%s" % alert_count(telid, clock(a), clock(b)))
 print("告警收敛后报警条数：%s" % dd_count(a, b))
+
+# redisid = 8
+# telid = 2
+
+
+print("查询时间范围：%s - %s" % (a, b))
+print("报警条数：%s" % alert_count(redisid, clock(a), clock(b)))
+
